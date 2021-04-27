ce3e4fbde5f85d778db75be3f82812a80aa31c06
EunJeong Heo <79428102+hej6853@users.noreply.github.com>
2021-04-27 04:50:02 -0700

    Delete Homework directory

--- Homework/PyBank/pybank.py
+++
@@ -1,8 +0,0 @@
-import os
-import csv
-
-csvpath = os.path.join("Resources", "budget_data.csv")
-
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
